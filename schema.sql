65c04e456e31488e3cf36d8fce9e2b27af7989c4
Zuzana Svorenova <zuzana.svorenova@gmail.com>
2020-08-29 23:42:54 -0500

    cleaning + schemas, issues with secondary key to be solved 0829

--- schema.sql
+++ schema.sql
@@ -1,7 +1,11 @@
+DROP TABLE IF EXISTS id_zip;
+DROP TABLE IF EXISTS staffing;
+
 CREATE TABLE id_zip (
 	hospital_id INT PRIMARY KEY NOT NULL,
 	facility_num INT NOT NULL,
 	hospital_name VARCHAR NOT NULL,
+	status VARCHAR NOT NULL,
 	address VARCHAR NOT NULL,
 	city VARCHAR NOT NULL,
     zip_code INT NOT NULL,
@@ -10,4 +14,15 @@ CREATE TABLE id_zip (
 	lat FLOAT NOT NULL
 );
 
+CREATE TABLE staffing (
+	hospital_id INT, 
+	year INT NOT NULL,
+	staff_group VARCHAR NOT NULL,
+	prod_hrs INT,
+	prodhrs_adjptday FLOAT
+	--FOREIGN KEY (hospital_id) REFERENCES id_zip(hospital_id)
+);
+
 SELECT * FROM id_zip LIMIT 15;
+
+SELECT * FROM staffing LIMIT 15;
