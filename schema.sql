ff89942b8cea699b145976fbfc23e545dc9223c4
Zuzana Svorenova <zuzana.svorenova@gmail.com>
2020-08-31 01:06:10 -0500

    better bridge df

--- schema.sql
+++ schema.sql
@@ -2,16 +2,19 @@ DROP TABLE IF EXISTS id_zip;
 DROP TABLE IF EXISTS staffing;
 
 CREATE TABLE id_zip (
-	hospital_id INT PRIMARY KEY NOT NULL,
-	facility_num INT NOT NULL,
-	hospital_name VARCHAR NOT NULL,
+	hospital_id INT PRIMARY KEY NOT NULL, 
+	hospital_name VARCHAR NOT NULL, 
 	status VARCHAR NOT NULL,
+	fac_level VARCHAR NOT NULL,
 	address VARCHAR NOT NULL,
 	city VARCHAR NOT NULL,
-    zip_code INT NOT NULL,
-	county TEXT NOT NULL,
+	zip_code INT NOT NULL,
+	county VARCHAR NOT NULL,
+	county_code INT NOT NULL,
+	category VARCHAR NOT NULL,
 	lng FLOAT NOT NULL,
-	lat FLOAT NOT NULL
+	lat FLOAT NOT NULL,
+	facility_num INT NOT NULL
 );
 
 CREATE TABLE staffing (
@@ -19,10 +22,11 @@ CREATE TABLE staffing (
 	year INT NOT NULL,
 	staff_group VARCHAR NOT NULL,
 	prod_hrs INT,
-	prodhrs_adjptday FLOAT
-	--FOREIGN KEY (hospital_id) REFERENCES id_zip(hospital_id)
+	prodhrs_adjptday FLOAT,
+	FOREIGN KEY (hospital_id) REFERENCES id_zip(hospital_id)
 );
 
 SELECT * FROM id_zip LIMIT 15;
 
 SELECT * FROM staffing LIMIT 15;
+
