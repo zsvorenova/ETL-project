d4f6c7823ca1a8265e3b490fb149f9ff1e35a627
Chioma Maduko <cbmaduko@gmail.com>
2020-08-31 22:20:04 -0500

    final submission

--- schema.sql
+++ schema.sql
@@ -1,18 +1,19 @@
-Create Table Profit(
-	year INT, 
-	Facility_Number INT Primary Key, 
-    Facility_Name VARCHAR (60) NOT NULL,
-	Begin_Date DATE,
-	End_Date DATE,
-	County_Name VARCHAR (60) NOT NULL,
-	Control_Type VARCHAR (60) NOT NULL,
-	Income_Statement_Item VARCHAR (60) NOT NULL, 
-	Income_Statement_Amount INT,
-	Patient_Adjusted_Day INT
+DROP TABLE IF EXISTS profit;
+
+Create Table profit (
+		year INT NOT NULL, 
+		hospital_id INT Primary Key NOT NULL,
+		facility_number INT NOT NULL,
+    	facility_name VARCHAR NOT NULL,
+		begin_date DATE NOT NULL,
+		end_date DATE NOT NULL,
+		county_name VARCHAR NOT NULL,
+		control_type VARCHAR NOT NULL,
+		income_statement_item VARCHAR NOT NULL, 
+		income_statement_amount FLOAT NOT NULL,
+		patient_adjusted_day INT NOT NULL                                                                                                                                                                                            
 );
 
 
-Select * from Profit 
-
-
+Select * from profit 
 
