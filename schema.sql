457340cee9eae0244404721244d4dcd796ac7bb3
OlegRyzhkov2020 <61919810+OlegRyzhkov2020@users.noreply.github.com>
2020-09-01 20:46:36 -0500

    updating table relationships

--- schema.sql
+++ schema.sql
@@ -87,17 +87,10 @@ CREATE TABLE healthcare_census(
 );
 
 Create Table hospitals_profit (
-		year INT NOT NULL, 
 		hospital_id INT Primary Key NOT NULL,
-		facility_number INT,
-    	facility_name VARCHAR NOT NULL,
-		begin_date DATE NOT NULL,
-		end_date DATE NOT NULL,
-		county_name VARCHAR NOT NULL,
-		control_type VARCHAR NOT NULL,
+		year INT NOT NULL, 
 		income_statement_item VARCHAR NOT NULL, 
-		income_statement_amount FLOAT NOT NULL,
-		patient_adjusted_day INT NOT NULL                                                                                                                                                                                            
-);
+		income_statement_amount FLOAT NOT NULL,                                                                                         	  FOREIGN KEY (hospital_id) REFERENCES ca_hospitals(hospital_id)                                                                                           
+);                                                                                                                                                                                          
 
 
